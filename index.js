ce832bc5d56dc241f717e530db3d34a065d31d21
BunMeat <varcmano57@gmail.com>
2024-06-18 01:43:16 +0700

    add route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-require('dotenv').config();
+require('dotenv').config(); 
+
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
