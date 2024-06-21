49ce36fed72e1a6fd8640af693996c6e8d4eac09
BunMeat <varcmano57@gmail.com>
2024-06-21 21:09:14 +0700

    remove firebases files from firebase folder to public/js

---
+++ public/js/firebase-config.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: window.env.FIREBASEKEY,
+  authDomain: window.env.FIREBASEAUTHDOMAIN,
+  projectId: window.env.PROJECTID,
+  storageBucket: window.env.STORAGEBUCKET,
+  messagingSenderId: window.env.MESSAGINGSENDERID,
+  appId: window.env.APPID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+const registrationForm = document.getElementById("registerForm");
+
+registrationForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth,email,password);
+    const user = userCredential.user;
+
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMsg = error.message;
+    console.error("Register Error", errorMsg);
+  }
+})
+export default firebaseConfig;
+
+
+
