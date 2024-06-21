83ee409df3ff2f5680297b7e801e1a96eec1b262
BunMeat <varcmano57@gmail.com>
2024-06-21 20:08:22 +0700

    added env.js

---
+++ public/js/env.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: window.env.FIREBASEKEY,
+  authDomain: window.env.FIREBASEAUTHDOMAIN,
+  projectId: window.env.PROJECTID,
+  storageBucket: window.env.STORAGEBUCKET,
+  messagingSenderId: window.env.MESSAGINGSENDERID,
+  appId: window.env.APPID
+};
