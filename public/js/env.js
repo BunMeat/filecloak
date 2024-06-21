1d35c63ae0e0cb4990dd29d3365003da5316a54e
BunMeat <varcmano57@gmail.com>
2024-06-21 20:53:18 +0700

    change script directory

--- public/js/env.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: window.env.FIREBASEKEY,
-  authDomain: window.env.FIREBASEAUTHDOMAIN,
-  projectId: window.env.PROJECTID,
-  storageBucket: window.env.STORAGEBUCKET,
-  messagingSenderId: window.env.MESSAGINGSENDERID,
-  appId: window.env.APPID
-};
