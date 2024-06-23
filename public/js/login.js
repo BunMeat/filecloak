1ee40975a4fc736b0478cdbbe6796d09708f2f16
BunMeat <varcmano57@gmail.com>
2024-06-23 23:22:10 +0700

    Revert "redefine firebaseConfig"
    This reverts commit a3b9aeb1df8052b4ade4b675142cd9fc21324448.

--- public/js/login.js
+++ public/js/login.js
@@ -2,54 +2,51 @@ import { initializeApp as initializeApp } from 'https://www.gstatic.com/firebase
 import { getAuth as getAuth, signInWithEmailAndPassword as signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 import { getFirestore as getFirestore, doc as doc, getDoc as getDoc } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
 
-async function getFirebaseConfig() {
-  const response = await fetch('/api/firebase-config');
-  return response.json();
-}
+const firebaseConfig = {
+  apiKey: window.env.FIREBASEKEY,
+  authDomain: window.env.FIREBASEAUTHDOMAIN,
+  projectId: window.env.PROJECTID,
+  storageBucket: window.env.STORAGEBUCKET,
+  messagingSenderId: window.env.MESSAGINGSENDERID,
+  appId: window.env.APPID
+};
 
-async function initializeFirebase() {
-  const firebaseConfig = await getFirebaseConfig();
-  const firebaseApp = initializeApp(firebaseConfig);
-  const auth = getAuth(firebaseApp);
-  const firestore = getFirestore(firebaseApp);
-
-  return { auth, firestore };
-}
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
 
 const loginForm = document.getElementById('loginForm');
 
-initializeFirebase().then(({ auth, firestore }) => {
-  loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-    const email  = document.getElementById('emailLogin').value;
-    const password  = document.getElementById('passwordLogin').value;
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-      const userDoc = await getDoc(doc(firestore, 'users', user.uid));
-      if (userDoc.exists()) {
-        const userData = userDoc.data();
-        alert("Sukses Login");
-        if (userData.role === "user") {
-          window.location.href = "../html/userPage.html";
-        } else {
-          window.location.href = "../html/adminPageEncrypt.html";
-        }
+  const email  = document.getElementById('emailLogin').value;
+  const password  = document.getElementById('passwordLogin').value;
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    const userDoc = await getDoc(doc(firestore, 'users', user.uid));
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      alert("Sukses Login");
+      if (userData.role === "user") {
+        window.location.href = "../html/userPage.html";
       } else {
-        alert("Email / Password Salah");
+        window.location.href = "../html/adminPageEncrypt.html";
       }
-    } catch (error) {
-      const errorCode = error.code;
-      const errorMsg = error.message;
-      console.error('Login error: ', errorMsg);
-      if (errorCode === 'auth/invalid-credential') {
-        alert("Email / Password Salah");
-      } else {
-        console.log("errorMsg", errorMsg);
-        console.log("errorCode", errorCode);
-        alert("Kesalahan Server", errorMsg);
-      }
+    } else {
+      alert("Email / Password Salah");
     }
-  });
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMsg = error.message;
+    console.error('Login error: ', errorMsg);
+    if (errorCode === 'auth/invalid-credential') {
+      alert("Email / Password Salah");
+    } else {
+      console.log("errorMsg", errorMsg);
+      console.log("errorCode", errorCode);
+      alert("Kesalahan Server", errorMsg);
+    }
+  }
 });
--- public/js/register.js
+++ public/js/register.js
@@ -2,61 +2,57 @@ import { initializeApp as initializeApp } from 'https://www.gstatic.com/firebase
 import { getAuth as getAuth, createUserWithEmailAndPassword as createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 import { getFirestore as getFirestore, collection as collection, doc as doc, setDoc as setDoc } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
 
-async function getFirebaseConfig() {
-  const response = await fetch('/api/firebase-config');
-  return response.json();
-}
+const firebaseConfig = {
+  apiKey: window.env.FIREBASEKEY,
+  authDomain: window.env.FIREBASEAUTHDOMAIN,
+  projectId: window.env.PROJECTID,
+  storageBucket: window.env.STORAGEBUCKET,
+  messagingSenderId: window.env.MESSAGINGSENDERID,
+  appId: window.env.APPID
+};
 
-async function initializeFirebase() {
-  const firebaseConfig = await getFirebaseConfig();
-  const firebaseApp = initializeApp(firebaseConfig);
-  const auth = getAuth(firebaseApp);
-  const firestore = getFirestore(firebaseApp);
-
-  return { auth, firestore };
-}
-
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
 
 // Registration Function
 const registrationForm = document.getElementById('registerForm');
 
-initializeFirebase().then(({ auth, firestore }) => {
-  registrationForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const repeatPassword = document.getElementById('repeatPassword').value;
+registrationForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const repeatPassword = document.getElementById('repeatPassword').value;
 
-    try {
-      if (repeatPassword !== password) {
-        alert("Password tidak sama");
-        return;
-      } else {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        console.log('User registered:', user);
+  try {
+    if (repeatPassword !== password) {
+      alert("Password tidak sama");
+      return;
+    } else {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log('User registered:', user);
 
-        const userCollection = collection(firestore, "users");
-        const userId = user.uid;
-        const userRefDoc = doc(userCollection, userId);
-        const userData = {
-          uid: user.uid,
-          role: "user",
-          email: email
-        };
-        await setDoc(userRefDoc, userData);
-        alert("Pendaftaran User Berhasil, Silahkan Login");
-        console.log('User registered and data saved to Firestore:', user);
-      }
-    } catch (error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      if (errorCode === 'auth/email-already-in-use') {
-        alert("Email Sudah Terdaftar");
-      } else {
-        alert("Kesalahan Server", errorMessage);
-      }
-      console.error('Registration error:', errorMessage);
+      const userCollection = collection(firestore, "users");
+      const userId = user.uid;
+      const userRefDoc = doc(userCollection, userId);
+      const userData = {
+        uid: user.uid,
+        role: "user",
+        email: email
+      };
+      await setDoc(userRefDoc, userData);
+      alert("Pendaftaran User Berhasil, Silahkan Login");
+      console.log('User registered and data saved to Firestore:', user);
     }
-  });
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    if (errorCode === 'auth/email-already-in-use') {
+      alert("Email Sudah Terdaftar");
+    } else {
+      alert("Kesalahan Server", errorMessage);
+    }
+    console.error('Registration error:', errorMessage);
+  }
 });
