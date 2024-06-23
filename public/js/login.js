a3b9aeb1df8052b4ade4b675142cd9fc21324448
BunMeat <varcmano57@gmail.com>
2024-06-23 23:14:31 +0700

    redefine firebaseConfig

--- public/js/login.js
+++ public/js/login.js
@@ -2,51 +2,54 @@ import { initializeApp as initializeApp } from 'https://www.gstatic.com/firebase
 import { getAuth as getAuth, signInWithEmailAndPassword as signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 import { getFirestore as getFirestore, doc as doc, getDoc as getDoc } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
 
-const firebaseConfig = {
-  apiKey: window.env.FIREBASEKEY,
-  authDomain: window.env.FIREBASEAUTHDOMAIN,
-  projectId: window.env.PROJECTID,
-  storageBucket: window.env.STORAGEBUCKET,
-  messagingSenderId: window.env.MESSAGINGSENDERID,
-  appId: window.env.APPID
-};
+async function getFirebaseConfig() {
+  const response = await fetch('/api/firebase-config');
+  return response.json();
+}
 
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
+async function initializeFirebase() {
+  const firebaseConfig = await getFirebaseConfig();
+  const firebaseApp = initializeApp(firebaseConfig);
+  const auth = getAuth(firebaseApp);
+  const firestore = getFirestore(firebaseApp);
+
+  return { auth, firestore };
+}
 
 const loginForm = document.getElementById('loginForm');
 
-loginForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
+initializeFirebase().then(({ auth, firestore }) => {
+  loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-  const email  = document.getElementById('emailLogin').value;
-  const password  = document.getElementById('passwordLogin').value;
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    const userDoc = await getDoc(doc(firestore, 'users', user.uid));
-    if (userDoc.exists()) {
-      const userData = userDoc.data();
-      alert("Sukses Login");
-      if (userData.role === "user") {
-        window.location.href = "../html/userPage.html";
+    const email  = document.getElementById('emailLogin').value;
+    const password  = document.getElementById('passwordLogin').value;
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      const userDoc = await getDoc(doc(firestore, 'users', user.uid));
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        alert("Sukses Login");
+        if (userData.role === "user") {
+          window.location.href = "../html/userPage.html";
+        } else {
+          window.location.href = "../html/adminPageEncrypt.html";
+        }
       } else {
-        window.location.href = "../html/adminPageEncrypt.html";
+        alert("Email / Password Salah");
       }
-    } else {
-      alert("Email / Password Salah");
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMsg = error.message;
+      console.error('Login error: ', errorMsg);
+      if (errorCode === 'auth/invalid-credential') {
+        alert("Email / Password Salah");
+      } else {
+        console.log("errorMsg", errorMsg);
+        console.log("errorCode", errorCode);
+        alert("Kesalahan Server", errorMsg);
+      }
     }
-  } catch (error) {
-    const errorCode = error.code;
-    const errorMsg = error.message;
-    console.error('Login error: ', errorMsg);
-    if (errorCode === 'auth/invalid-credential') {
-      alert("Email / Password Salah");
-    } else {
-      console.log("errorMsg", errorMsg);
-      console.log("errorCode", errorCode);
-      alert("Kesalahan Server", errorMsg);
-    }
-  }
+  });
 });
