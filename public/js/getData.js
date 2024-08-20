a101d9242dd49ad895f415aee79de506c05aa5ab
BunMeat <varcmano57@gmail.com>
2024-08-21 02:04:55 +0700

    Revert "test"
    This reverts commit 15e9b71f20254ef2276b5cf17514c80af9a61ab3.

--- public/js/getData.js
+++ public/js/getData.js
@@ -2,12 +2,12 @@ import { initializeApp as initializeApp} from 'https://www.gstatic.com/firebasej
 import { getFirestore as getFirestore, collection as collection, getDocs as getDocs } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: window.env.FIREBASEKEY,
+  authDomain: window.env.FIREBASEAUTHDOMAIN,
+  projectId: window.env.PROJECTID,
+  storageBucket: window.env.STORAGEBUCKET,
+  messagingSenderId: window.env.MESSAGINGSENDERID,
+  appId: window.env.APPID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
--- public/js/login.js
+++ public/js/login.js
@@ -3,12 +3,12 @@ import { getAuth as getAuth, signInWithEmailAndPassword as signInWithEmailAndPas
 import { getFirestore as getFirestore, doc as doc, getDoc as getDoc } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: window.env.FIREBASEKEY,
+  authDomain: window.env.FIREBASEAUTHDOMAIN,
+  projectId: window.env.PROJECTID,
+  storageBucket: window.env.STORAGEBUCKET,
+  messagingSenderId: window.env.MESSAGINGSENDERID,
+  appId: window.env.APPID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
