e977585897d7dcad12e7546f229e8ade8e083ab1
BunMeat <varcmano57@gmail.com>
2024-09-22 01:19:51 +0700

    fix import

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 import { initializeApp as initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js';
 import { getAuth as getAuth, signInWithEmailAndPassword as signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
-import { getFirestore as getFirestore, doc as doc, getDoc as getDoc, collection, setDoc, doc, query, where, getDocs, updateDoc  } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
+import { getFirestore as getFirestore, doc as doc, getDoc as getDoc, collection, query, where, getDocs, updateDoc  } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: window.env.FIREBASEKEY,
