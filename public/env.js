3d8a97c97d5b6dd7a1f64bacbce6d6e558fab84e
BunMeat <varcmano57@gmail.com>
2024-09-22 18:23:16 +0700

    add admin cred

--- public/env.js
+++ public/env.js
@@ -4,5 +4,15 @@ window.env = {
   PROJECTID: 'filecloak',
   STORAGEBUCKET: 'filecloak.appspot.com',
   MESSAGINGSENDERID: '370477539338',
-  APPID: '1:370477539338:web:5ad4ca5fbeddd2ce8cbff3'
+  APPID: '1:370477539338:web:5ad4ca5fbeddd2ce8cbff3',
+  TYPE: "service_account",
+  PRIVATE_KEY_ID: "1be5c13bade3d5b9ef4b5621aaebea3fa9e49ef8",
+  PRIVATE_KEY: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDP64otbDJ4E0Op\nTUg1IrEoo3GD6jfUO0WrlR0P6LJcRfEfrKR9GeC7p4Wwp8Qmdz9PyGGZ5oGmkjv3\n+w1jjEbu9TXdcoXknGCKWLtk5Xdqf4sLZzD0swOSgySBhV8KWigXP5O6BMw7qZqI\nlhfitXX0vVa2E1ro9ZO6nMcKRzH179DhgJVE9EhbRyVI3LzjKrdXvElzsxd7FxQY\njbglqft/240bN/ez9V+gSjQSbH1l965JzMsc9XK/cmMC1aMFdngojrcDJJre7uHk\nEQq54BuI0mbOsTB4hCtitQIy/1JWwbBgW6G4EPubnrZ3mopH9sTNcjTDFAEIvPFu\neBFeKtlDAgMBAAECggEAY7HMz3rRuEGEb+ledhvsVlAIXfgwTNMe1ObqyHYLD7N5\nqnZdaJ6/4zqwH8RRiSAke1zZb7HW+MeleYInKVRZrrpk2YnQKY3RvIRPVlGwsJ/3\nS8Bu+OXdEKNRHcmViKEyF/OkIc2rH/By6IsPaOUZagBfU250aL9X1mLWDVeV3RYC\n6kia5Qdgsq4lH35BvkqDePsTSF53m/mFTwaYEepWDawk2MBjzuTDcUkJYDbCqB1F\n5eXWI9FJuNP3PpnE2nQlEwxaa6TVF4SJAIuYUXjusYPmAVm3R08XS8ICbB0wnyzp\nQvuvuFEo2xz4aig6BkE4g5k3r8+QIK0ne5H9THYyAQKBgQDvlEZ9ofQieQ6VRJi3\nxLyQ/im1CCU9ENPkfFVWh1hmqMpTM5VQpnXBaZI62HinYWOLfCmdgfZzEd6ZTGWS\n2Q+AhfsYM+r1LG0G3CH6ctqoj4myRxVVUsxynNjM+JRHupPCY4TveslHb/k0cLN9\ntRBcJXM/4WlWXdyOSkixITKAhwKBgQDeK8OyHcSp16wduOXmYqHApr/XIRy2fBxV\nFJAtR7/KFiMQJ3Zc5D1AtMrJcb1T8Qb8t2IJjR5upo60L0Ksr1OuD6ow3MmLIurp\nESamMxFwitwQxafoby5ZJAZTF7/LB92RT/WQ9+3BXabpuXi9/MeoOdbZLK/hipKi\nayWHGA28ZQKBgFH0pCSICW7dgJqZa7C5sVPcTkIqdiqsnf+0dLDz/c6ogQXzSGnd\nd7zhM2/D64o+q4HkyIukEL2OaFqCouCYNlBeBOZjpEWYm/dTyJ/XOGufJpiA7pBQ\nkbXHZ/a+NmNPNQqzZp9Rnpc8e8BvqUcgcGdFPGam4pTz6OYYDUPiulbzAoGANYxu\n2pt6L0WgYnmST/kt63VFf+CnPwSi/465QXCttVOol9grGzCX4uSwi+UyMO1DNd4t\nbwfocdsLQjqx41VO4gE8kBJnMXX1EL/lhOGCLpIJgLFjSw6hKTMZoyVyQMlgigmv\nsoVPdO/RrzvY8lHe/ZkTQ70Vl1MDDMqufOU7SlECgYB4cetz9bKuJZrBUl8taVkS\npraTyIOaLqiB66pySObn7n9QrfnnVrtL3Y113vSGobYaf53WlwUi+P7BrjgAY7NG\nXRArF0EVsH5DwrhVJsExMhQBGhV8331otSQi171O2zk+QPeAUSJvEr4jPr+A1dIL\nX6d0XSqh1+8wl3+VmyEIWQ==\n-----END PRIVATE KEY-----\n",
+  CLIENT_EMAIL: "firebase-adminsdk-eylw5@filecloak.iam.gserviceaccount.com",
+  CLIENT_ID: "107737538124100094789",
+  AUTH_URI: "https://accounts.google.com/o/oauth2/auth",
+  TOKEN_URI: "https://oauth2.googleapis.com/token",
+  AUTH_PROVIDER_X509_CERT_URL: "https://www.googleapis.com/oauth2/v1/certs",
+  CLIENT_X509_CERT_URL: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-eylw5%40filecloak.iam.gserviceaccount.com",
+  UNIVERSE_DOMAIN: "googleapis.com"
 };
